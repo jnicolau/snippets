c68a7ae3fa267994eed67081d64ca5e8a0acf601
unknown <joliveira@JOLIVEIRAW7M.dubtilt.local>
2014-05-22 16:13:41 +0100

    commit

--- UnitTestsForHttpContext.cs
+++ UnitTestsForHttpContext.cs
@@ -1,11 +1,12 @@
-            _controller = new PlayChipStoreController(_mapper, _session, _paymentService, _middleware, _marketingService, _geoAddressService, _fieldBuilder, _cashier);
-            _controller.ControllerContext = new ControllerContext()
-                                                {
-                                                    HttpContext = FakeHttpContext.Generic(),
-                                                    RequestContext = new RequestContext(),
-                                                    RouteData = new RouteData()
-                                                };
-            _controller.ValueProvider = new FormCollection()
-            {
-                //{ "Some key", "Some Value" },
-            }.ToValueProvider();
+            // Setup Controller
+            _controller = new PlayChipStoreController(_mapper, _session, _paymentService, _middleware, _marketingService,
+                                                      _geoAddressService, _fieldBuilder, _cashier)
+                              {
+                                  ControllerContext = new ControllerContext
+                                                          {
+                                                              HttpContext = FakeHttpContext.Generic(),
+                                                              RequestContext = new RequestContext(),
+                                                              RouteData = new RouteData()
+                                                          },
+                                  ValueProvider = new FormCollection() { /*{ "Some key", "Some Value" }, */}.ToValueProvider()
+                              };
